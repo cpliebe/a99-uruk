9a553514c0b890c72fa91428d0ba41cea71e3d9f
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-04-18 12:51:15 -0400

    Added boilerplate node.js setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const minimist = require('minimist');
+const fs = require('fs');
+const db = require("./src/services/database.js");
+const morgan = require('morgan');
+
+const app = express()
+const argv = (minimist)(process.argv.slice(2));
+
+// Set valid arguments
+argv["port"];
+
+// Get port from argument, if not argument exists set to 5000
+const HTTP_PORT = argv.port || 5000;
+
+
+// Listen on port
+const server = app.listen(HTTP_PORT, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+});
+
+
+// Default endpoint request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
