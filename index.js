166bca4163cc2d99014e9d493c36a72b12fa4c66
rceglie <rceglie@email.unc.edu>
2022-04-22 13:36:45 -0400

    Restructured page navigation endpoints into src/routes instead of taking up space in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const user_db = require("./src/services/user-database.js");
 const interactions_db = require("./src/services/interactions-database.js");
 const morgan = require('morgan');
+const path = require('path')
 const app = express()
 
 // Serve login page as start page
@@ -22,24 +23,9 @@ const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 
-// Endpoints for directory to EXAMPLEPAGE ----------- (should try to put this in a separate folder/file)
+// EXAMPLEPAGE route
+app.use(require("./src/routes/examplepage.route"))
 
-// Returns html
-app.get('/examplepage/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public/examplepage/index.html'));
-})
-
-// Returns css
-app.get('/examplepage/style.css', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public/examplepage/style.css'));
-})
-
-// Returns js
-app.get('/examplepage/script.js', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public/examplepage/script.js'));
-})
-
-// ---------------------------------------------------
 
 // Signup Endpoint
 app.get('/sign-up', (req, res) => {
